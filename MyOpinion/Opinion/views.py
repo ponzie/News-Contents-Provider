31979f49c78c1d686191268b1812e12724c8f1ea
aiti <aiti@aiti-237.(none)>
2011-06-27 11:47:50 +0000

    Send

--- MyOpinion/Opinion/views.py
+++ MyOpinion/Opinion/views.py
@@ -1,3 +1,19 @@
+
+def home(request)
+    t = loader.get_template('Opinion/home.html')
+    c = Context(dict())
+    return HttpResponse(t.render(c))
+
+
+def search(request,term)
+    opinion_list = Opinion.object.filter(title__icontains=term)
+    t = loader.get_template('Opinion/search.html')
+    c = Context({'opinion_list':opinion_list , 'term':term })
+    for opinion in opinion_list 
+        print Opinion.title
+    return HttpResponse(t.render(c))
+
+
 def blog_editcomment(request, id):
 	comment = Comment.objects.get(pk=id)
 	if request.method == 'POST':
