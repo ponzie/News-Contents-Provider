e64e4e2ccce786e3a2701b9f568d7a1f9b157626
cynthia owusu <cynthia@cynthia-NV53.(none)>
2011-06-27 11:05:11 +0000

    deleted extraneous folders and changed models

--- MyOpinion/Opinion/models.py
+++ MyOpinion/Opinion/models.py
@@ -23,6 +23,26 @@ class OpinionAdmin(admin.ModelAdmin):
 		return self.list_display 
 
 	
+class Comment(models.Model):
+	body=  models.TextField()
+        author=models.CharField(max_length=60)
+	created    = models.DateTimeField(auto_now=True)
+    	updated    = models.DateTimeField(auto_now=True)
+	link= models.ForeignKey(Opinion)
+	def __unicode__(self):
+		return str(self.post)+","+self.author
+	def body_first_70(self):
+		return self.body[:60]
+
+class CommentInline(admin.TabularInline):
+	model = Comment
+
+class CommentAdmin(admin.ModelAdmin):
+	list_display=('link','author','created','updated')
+	list_display = ('body_first_70',)
+	list_filter = ('created','author')
+
+admin.site.register(Comment,CommentAdmin)
 admin.site.register(Opinion, OpinionAdmin)
 
 	
